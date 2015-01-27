ac6790ebe4cd6e07b24a84ef15bd6ae53c0ecd4d
Floris Briolas <f.briolas@codehesion.nl>
2015-01-27 09:04:04 +0100

    + CodedUi for basic & shared

---
+++ CodedUI_Calculator/Basic.cs
@@ -0,0 +1,22 @@
+﻿using CodedUI_Calculator.SharedClasses;
+
+namespace CodedUI_Calculator.BasicClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.CodeDom.Compiler;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.Text.RegularExpressions;
+    
+    
+    public partial class Basic : Shared
+    {
+    }
+}
